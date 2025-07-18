7332846597c8fce9ff16ae1686f68f05c27c29f3
blakpat <103948183+blakpat@users.noreply.github.com>
2025-07-18 12:51:45 +0200

    Procedimiento actualizado

---
+++ Intune - Mapear unidades de red Azure Blob Storage/Scripts/cmdkeyUnistall.ps1
@@ -0,0 +1,20 @@
+﻿try {
+    # Eliminar la clave de cmdkey
+    cmd.exe /C "cmdkey /delete:`"CHANGE_ME`""
+    $log = "$(Get-Date): Clave eliminada correctamente para CHANGE_ME"
+} catch {
+    $log = "$(Get-Date): ERROR al eliminar la clave: $_"
+}
+
+try {
+    # Eliminar la carpeta de logs si existe
+    if (Test-Path -Path "C:\GPO_FileShared") {
+        Remove-Item -Path "C:\GPO_FileShared" -Recurse -Force
+        $log += "`n$(Get-Date): Carpeta de logs C:\GPO_FileShared eliminada correctamente."
+    } else {
+        $log += "`n$(Get-Date): Carpeta de logs C:\GPO_FileShared no existe."
+    }
+} catch {
+    $log += "`n$(Get-Date): ERROR al eliminar la carpeta de logs: $_"
+}
+
---
+++ Intune - Mapear unidades de red Azure Blob Storage/Scripts/cmdkeyinstall.ps1
@@ -0,0 +1,10 @@
+﻿if (!(Test-Path -Path "C:\GPO_FileShared")) {
+    New-Item -Path "C:\GPO_FileShared" -ItemType Directory
+}
+
+try {
+    cmd.exe /C "cmdkey /add:`"CHANGE_ME`" /user:`"CHANGE_ME`" /pass:`"CHANGE_ME`""
+    Add-Content -Path "C:\GPO_FileShared" -Value "$(Get-Date): Clave insertada correctamente para CHANGE_ME"
+} catch {
+    Add-Content -Path "C:\GPO_FileShared\LogFile.log" -Value "$(Get-Date): ERROR al insertar la clave: $_"
+}
